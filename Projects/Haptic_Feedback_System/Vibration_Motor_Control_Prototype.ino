d130bc904c573c3d35c9ed0e7c46a49bc95f9770
J3Droid <67392334+J3Droid@users.noreply.github.com>
2022-10-10 18:51:00 -0400

    Add files via upload

--- Projects/Haptic_Feedback_System/Vibration_Motor_Control_Prototype.ino
+++ Projects/Haptic_Feedback_System/Vibration_Motor_Control_Prototype.ino
@@ -8,6 +8,7 @@ int vibrator = 6;
 // Global Variables
 int intensity;
 int timestep = 0;
+int count = 0;
 
 void setup() 
 {
@@ -16,58 +17,56 @@ void setup()
   Serial.begin(9600);
 }
 
-//void absWave()
-//{
-//   for (intensity = 0; intensity < 150; intensity++)
-//  {
-//     analogWrite(vibrator, intensity);
-//     Serial.println(intensity);
-//     delay(20);
-//  }
-//  delay(100);
-//  for (intensity = 150; intensity > 0; intensity--)
-//  {
-//     analogWrite(vibrator, intensity);
-//     Serial.println(intensity);
-//     delay(20);
-//  }
-//  delay(100);
-//}
-//
-//void sinWave()
-//{
-//  intensity = 75 * sin(0.1*timestep) + 75;
-//  analogWrite(vibrator, intensity);
-//  Serial.println(intensity);
-//  timestep++;
-//  delay(50);
-//}
+void absWave()
+{
+   for (intensity = 0; intensity < 150; intensity++)
+  {
+     analogWrite(vibrator, intensity);
+     Serial.println(intensity);
+     delay(20);
+  }
+  delay(100);
+  for (intensity = 150; intensity > 0; intensity--)
+  {
+     analogWrite(vibrator, intensity);
+     Serial.println(intensity);
+     delay(20);
+  }
+  delay(100);
+}
+
+void sinWave()
+{
+  intensity = 75 * sin(0.1*timestep) + 75;
+  analogWrite(vibrator, intensity);
+  Serial.println(intensity);
+  timestep++;
+  delay(50);
+}
 
 void testIntensity()
 {
-  if (intensity = 0)
+  if (count %2 == 0)
   {
-    
+    Serial.println("Enter intensity value (0-255):");
   }
-  Serial.println("Enter intensity value (0-255):");
-  delay(500);
   while (Serial.available() == 0) {}
   intensity = Serial.parseInt();
+  count++;
   
-  if (intensity > 0 && intensity <= 255)
+  if (intensity > 0 && intensity <= 255 && count%2 == 1)
   {
-    Serial.println("0");
-    Serial.println("Vibrating at an intensity of " + String(intensity));
-    Serial.println("1");
-    analogWrite(vibrator, intensity);
-    Serial.println("2");
-    delay(1000);
-    Serial.println("3");
-    intensity = 0;
-    analogWrite(vibrator, 0);
-    Serial.println("4");
+    Serial.println("This is what " + String(intensity) + " feels like");
+    for (int i = 0; i < 3; i++)
+    {
+      analogWrite(vibrator, intensity);
+      delay(250);
+      analogWrite(vibrator, 0);
+      delay(250); 
+    }
   }
   
+  else if (count%2 == 0){}
   else
   {
     Serial.println("Invalid input!");
@@ -81,3 +80,4 @@ void loop()
 }
 
 // Optimal intensity range appears to be ~50-150.
+// Ideal pulse intensity is 105.
