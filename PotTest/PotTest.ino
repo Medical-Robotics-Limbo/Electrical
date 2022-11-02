5aa86ad42630b341e4d2a1c1adf656060bb9ee81
Param Pithadia <ppithadia6@gatech.edu>
2022-11-02 19:57:29 -0400

    Update pot test to include motor feedback loop
    Not yet working, only saves initial pot values

--- PotTest/PotTest.ino
+++ PotTest/PotTest.ino
@@ -1,14 +1,30 @@
-uint8_t pin = A0;
+uint8_t potPin = A0;
+uint8_t motorPin = 1;
 uint8_t angle;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin, INPUT);
+  pinMode(potPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+  analogWrite(motorPin, 0);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  angle = map(analogRead(pin), 0, 1023, 0, 330);  
-  Serial.println(angle);
+  updateAngle(); 
+  setMotor(angle, 90); 
+}
+
+void setMotor(uint8_t angle, uint8_t pos){
+  while (angle != pos) {
+    updateAngle();
+    Serial.println(angle);
+    analogWrite(motorPin, 256);
+  }
+  analogWrite(motorPin, 0);
+}
+
+void updateAngle(){
+  angle = map(analogRead(potPin), 0, 1023, 0, 330);
 }
