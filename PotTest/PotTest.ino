bbe34472f7fe4494cc9fa77559cedb546bb6bca8
Param Pithadia <ppithadia6@gatech.edu>
2022-11-14 19:46:14 -0500

    Motor Controll Added

--- PotTest/PotTest.ino
+++ PotTest/PotTest.ino
@@ -1,30 +1,52 @@
-uint8_t potPin = A0;
-uint8_t motorPin = 1;
+const uint8_t potPin = A0;
+
+const uint8_t mot1 = 7;
+const uint8_t mot2 = 6;
+
 uint8_t angle;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(potPin, INPUT);
-  pinMode(motorPin, OUTPUT);
-  analogWrite(motorPin, 0);
+  pinMode(mot1, OUTPUT);
+  pinMode(mot2, OUTPUT);
+  stopMotor();
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   updateAngle(); 
-  setMotor(angle, 90); 
-}
-
-void setMotor(uint8_t angle, uint8_t pos){
-  while (angle != pos) {
-    updateAngle();
-    Serial.println(angle);
-    analogWrite(motorPin, 256);
-  }
-  analogWrite(motorPin, 0);
+  if (angle > 200) {
+    stopMotor();
+    }
+  else {
+    spinMotor(true);
+    }
+//  spinMotor(true); 
+//  delay(1000);
+//  spinMotor(false);
+//  delay(1000);
+//  spinMotor(false);
 }
 
 void updateAngle(){
   angle = map(analogRead(potPin), 0, 1023, 0, 330);
+  Serial.println(angle);
 }
+
+void spinMotor(bool dir) {
+  if (dir) {
+    digitalWrite(mot1, HIGH);
+    digitalWrite(mot2, LOW);
+    }
+  else {
+    digitalWrite(mot1, LOW);
+    digitalWrite(mot2, HIGH);
+    }
+  }
+
+void stopMotor() {
+  digitalWrite(mot1, LOW);
+  digitalWrite(mot2, LOW);
+  }
